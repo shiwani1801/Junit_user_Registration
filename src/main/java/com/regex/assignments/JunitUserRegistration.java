8b47425f16c1ee75af47644c895f0199b862573c
shiwani <shiwanipaunikar1@gmail.com>
2022-12-29 13:53:59 -0800

    [shiwani]junit user registration program

---
+++ src/main/java/com/regex/assignments/JunitUserRegistration.java
@@ -0,0 +1,9 @@
+package com.regex.assignments;
+
+import java.util.Scanner;
+
+public class JunitUserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to junit user registration");
+    }
+}
