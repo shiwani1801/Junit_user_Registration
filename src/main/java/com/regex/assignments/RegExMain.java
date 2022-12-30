d40225f95377cce69402c86ec6aaaa513cafba7e
shiwani <shiwanipaunikar1@gmail.com>
2022-12-29 21:22:00 -0800

    [shiwani]user need to check all sample email

--- src/main/java/com/regex/assignments/RegExMain.java
+++ src/main/java/com/regex/assignments/RegExMain.java
@@ -3,13 +3,18 @@ package com.regex.assignments;
 import java.util.regex.Pattern;
 
 public class RegExMain {
+    public String firstName = "Soumya";
+    public String lastName = "Ranjan";
+    public String emailId = "soumyars675@gmail.com";
+    public String phoneNo = "+91 8908641811";
+    public String password = "Soumya@123";
     public boolean validateFirstName(String firstName) {
         return Pattern.matches("[A-Z][a-z]{3,}", firstName);
     }
     public boolean validateLastName(String lastName) {
         return Pattern.matches("[A-Z][a-z]{3,}",lastName);
     }
-    public boolean validateEmail(String email) {
+    public static boolean validateEmail(String email) {
         return Pattern.matches("^[a-z0-9+_.-]+@+[a-z]+.+[a-z]+.+[a-z]$",email);
     }
     public boolean validatePhoneNo(String phoneNo) {
--- src/test/java/com/regex/test/RegExTest.java
+++ src/test/java/com/regex/test/RegExTest.java
@@ -5,9 +5,13 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegExTest {
 
     RegExMain obj = new RegExMain();
+    List<String> emailList = new ArrayList<>();
 
     @BeforeClass
     public static void welcomeText() {
@@ -147,6 +151,31 @@ public class RegExTest {
         System.out.println("Invalid password of user which doesn't contains special character");
         String password = "Soumya123";
         Assert.assertFalse(obj.validatePassword(password));
+    }
+    @Test
+    public void validateSampleEmailTest() {
+        System.out.println("Validate some sample email ids");
+        emailList.add("abc@yahoo.com");
+        emailList.add("abc-100@yahoo.com");
+        emailList.add("abc111@abc.com");
+        emailList.add("abc.100@yahoo.com");
+        emailList.add("abc-100@abc.net");
+        emailList.add("abc.100@abc.com.au");
+        emailList.add("abc@1.com");
+        emailList.add("abc@gmail.com.com");
+        emailList.add("abc+100@gmail.com");
+        for (int i = 0; i < emailList.size(); i++)
+        {
+            try
+            {
+                RegExMain.validateEmail(emailList.get(i));
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                emailList.remove(i);
+            }
+        }
     }
 }
 
